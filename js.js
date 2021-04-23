626df3ba3ff2bc67c8c8c2919a143a2c1833e5c3
daniilkukalo <56275096+daniilkukalo@users.noreply.github.com>
2021-04-23 19:07:55 +0300

    lop

--- js.js
+++ js.js
@@ -3,19 +3,47 @@ $(document).ready(function () {
 	//ScrollMagic controller init
 	let controller = new ScrollMagic.Controller();
 
-	var wipeAnimation = new TimelineMax()
-			.fromTo('.project2', {y: '-100%', x: '-100%'}, {y: '0%', x: '0%', ease: Linear.easeNone})
-    .fromTo('.project3', {y: '100%', x: '100%'}, {x: '0%', y: '0%', ease: Linear.easeNone})
-    .fromTo('.project4', {y: '-100%', x: '-100%'}, {y: '0%', x: '0%', ease: Linear.easeNone});
+	let pinHeaderScene = new ScrollMagic.Scene({
+		triggerElement: '.header',
+		triggerHook: 0,
+		duration: '35%'
+	})
+	.setPin(".header", {pushFollowers: false})
+	.addTo(controller)
 
-		// create scene to pin and link animation
-		new ScrollMagic.Scene({
-			triggerElement: "#main",
-			triggerHook: "onLeave",
-			duration: "300%"
-		})
-		.setPin("#main")
-		.setTween(wipeAnimation)
-		.addIndicators() // add indicators (requires plugin)
-		.addTo(controller);
-	});
+	let pinHeaderScene2 = new ScrollMagic.Scene({
+		triggerElement: '.project1',
+		triggerHook: 0.3,
+	})
+	.setPin(".header", {pushFollowers: true})
+	//.addIndicators()
+	.addTo(controller)
+
+	// Parallax Scenes
+	let slideParallaxScene = new ScrollMagic.Scene({
+		triggerElement: '.parallax1',
+		triggerHook: 0.7,
+		duration: '200%'
+	})
+	.setTween(".bcg1", 1, {y: '80%', ease: Linear.easeNone})
+// 	.addIndicators()
+	.addTo(controller);
+
+	let slideParallaxScene2 = new ScrollMagic.Scene({
+		triggerElement: '.parallax2',
+		triggerHook: 0.7,
+		duration: '200%'
+	})
+	.setTween(".bcg2", 1, {y: '80%', ease: Linear.easeNone})
+// 	.addIndicators()
+	.addTo(controller);
+
+	let slideParallaxScene3 = new ScrollMagic.Scene({
+		triggerElement: '.parallax3',
+		triggerHook: 0.7,
+		duration: '150%'
+	})
+	.setTween(".bcg3", 1, {y: '60%', ease: Linear.easeNone})
+// 	.addIndicators()
+	.addTo(controller);
+});
